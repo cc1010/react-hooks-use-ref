510406b2ab34b51a8e2c93adaca0adc58a90c4d5
chaim <chaimyisroel@gmail.com>
2021-09-13 21:01:18 -0400

    commit tree 652815274b613a99c5521bf1804a5538b38787e6

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,23 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { makeRandomNumber } from "../utils";
 
 function Ticker() {
-  const [price, setPrice] = useState(0);
-  const [color, setColor] = useState("black");
+	const [price, setPrice] = useState(0);
+	const [color, setColor] = useState("black");
+	// create the ref and set its initial value
+	const prevPriceRef = useRef(price);
 
-  useEffect(() => {
-    const id = setInterval(() => setPrice(makeRandomNumber), 1000);
-    return function () {
-      clearInterval(id);
-    };
-  }, []);
+	useEffect(() => {
+		// use the current value of the ref
+		const prevPrice = prevPriceRef.current;
+		if (price > prevPrice) {
+			setColor("green");
+		} else if (price < prevPrice) {
+			setColor("red");
+		} else {
+			setColor("black");
+		}
+		// set the new value of the ref (note: this doesn't trigger a re-render)
+		prevPriceRef.current = price;
+	}, [price]);
 
-  return (
-    <div>
-      <h1>TickerMaster</h1>
-      <h2 style={{ color: color }}>Price: ${price}</h2>
-    </div>
-  );
+	useEffect(() => {
+		const id = setInterval(() => setPrice(makeRandomNumber), 1000);
+		return function cleanup() {
+			clearInterval(id);
+		};
+	}, []);
+
+	return (
+		<div>
+			<h1>TickerMaster</h1>
+			<h2 style={{ color: color }}>Price: ${price}</h2>
+		</div>
+	);
 }
 
 export default Ticker;
